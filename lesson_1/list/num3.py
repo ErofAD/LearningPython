6b2e5d8e9eac9b659e0ec8645386bffb88696f5d
Anastasiia <36659899+erofad@users.noreply.github.com>
2018-09-13 22:32:16 +0300

    list

--- lesson_1/list/num3.py
+++ lesson_1/list/num3.py
@@ -7,24 +7,24 @@
         if my_list[iter]>my_list[iter-1]:
             
 '''
-def num3():
-    my_list = [1, 3, 5, 2, 3, 8, 10, 9, 6, 5, 7, 9, 0]
-    num_max = 0
+def num3(my_list):
+    #my_list = [1, 3, 5, 2, 3, 8, 10, 9, 4, 5, 7, 9, 10]
+    #my_list = [1, 2, 3]
     sum_max = 0
     num = 0
     s = 1
     for i in range(1, len(my_list)):
         if my_list[i] > my_list[i-1]:
             s += 1
+            if i == len(my_list) - 1 and sum_max < s:
+                sum_max = s
+                num = i - sum_max + 1
         else:
             if sum_max < s:
                 sum_max = s
                 num = i-sum_max
             s = 1
 
-    print(my_list[num:num+sum_max])
-
-if __name__=="__main__":
-    num3()
+    return (my_list[num:num+sum_max])
 
 
--- lesson_1/list/num5.py
+++ lesson_1/list/num5.py
@@ -1,8 +1,8 @@
 '''Найти максимальный и минимальный элемент последовательности [1, -3, -6, 2, 4, 0, -5, 5, 3]'''
 def num5():
     my_list = [1, -3, -6, 2, 4, 0, -5, 5, 3]
-    print("max = ", max(*my_list))
-    print("min = ", min(*my_list))
+    print("max = ", max(my_list))
+    print("min = ", min(my_list))
 
 if __name__ == "__main__":
     num5()
