3393304f91fbee1b583022ef44961004ab03aa10
ErofAD <efor.ad97@gmail.com>
2018-09-13 23:04:33 +0300

    tests

--- lesson_1/list/testList.py
+++ lesson_1/list/testList.py
@@ -3,11 +3,11 @@ from lesson_1.list.num3 import num3
 
 
 class TestList (unittest.TestCase):
-    def first_test_num3(self):
+    def test_num3_first(self):
         my_list = [1, 3, 5, 2, 3, 8, 10, 9, 6, 5, 7, 9, 0]
         self.assertEqual(num3(my_list), [2, 3, 8, 10])
 
-    def second_test_num3(self):
+    def test_num3_second(self):
         self.assertEqual(num3([1, 2, 3]), [1, 2, 3])
 
 if __name__ == '__main__':
